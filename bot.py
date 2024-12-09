c99ce5e5454fd0e15de356df3b4ad180fa92cb09
Aleksey Shcherbina <aleksey@ok-autoparts.ru>
2024-12-09 18:30:06 +0500

    Первый комит

---
+++ bot.py
@@ -0,0 +1,49 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.enums.dice_emoji import DiceEmoji
+from settings import TOKEN
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=TOKEN)
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Хэндлер на команду /test1
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+@dp.message(Command("answer"))
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
+
+
+@dp.message(Command("reply"))
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')    
+
+# Хэндлер на команду /test2
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")    
+
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message, bot: Bot):
+    await bot.send_dice(5282544717, emoji=DiceEmoji.DICE)
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+    # Где-то в другом месте, например, в функции main():
+    dp.message.register(cmd_test2, Command("test2"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
