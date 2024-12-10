993e902886e7051aa8f2917ee3a3ddcdd0906e97
Aleksey Shcherbina <aleksey@ok-autoparts.ru>
2024-12-10 11:47:17 +0500

    Добавлено все из статьи, часть кода не рабоает.

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.enums.dice_emoji import DiceEmoji
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.
+#from aiogram import F
+from magic_filter import F
 from config_reader import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -15,10 +19,79 @@ bot = Bot(token=config.bot_token.get_secret_value())
 # Диспетчер
 dp = Dispatcher()
 
+"""
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
+"""
+
+# Нажатие на одну из кнопок: -2, -1, +1, +2
+@dp.callback_query(NumbersCallbackFactory.filter(F.action == "change"))
+async def callbacks_num_change_fab(
+        callback: types.CallbackQuery, 
+        callback_data: NumbersCallbackFactory
+):
+    # Текущее значение
+    user_value = user_data.get(callback.from_user.id, 0)
+
+    user_data[callback.from_user.id] = user_value + callback_data.value
+    await update_num_text_fab(callback.message, user_value + callback_data.value)
+    await callback.answer()
+
+
+# Нажатие на кнопку "подтвердить"
+@dp.callback_query(NumbersCallbackFactory.filter(F.action == "finish"))
+async def callbacks_num_finish_fab(callback: types.CallbackQuery):
+    # Текущее значение
+    user_value = user_data.get(callback.from_user.id, 0)
+
+    await callback.message.edit_text(f"Итого: {user_value}")
+    await callback.answer()
+
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="С пюрешкой"),
+            types.KeyboardButton(text="Без пюрешки")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи"
+    )
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+# Обработка кнопок клавиатуры выше
+@dp.message(F.text.lower() == "с пюрешкой")
+async def with_puree(message: types.Message):
+    await message.reply("Отличный выбор!")
+
+@dp.message(F.text.lower() == "без пюрешки")
+async def without_puree(message: types.Message):
+    await message.reply("Так невкусно!")
+
+# Напишем хэндлер, который по команде /random будет отправлять сообщение с колбэк-кнопкой:
+@dp.message(Command("random"))
+async def cmd_random(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Нажми меня",
+        callback_data="random_value")
+    )
+    await message.answer(
+        "Нажмите на кнопку, чтобы бот отправил число от 1 до 10",
+        reply_markup=builder.as_markup()
+    )
+
+@dp.callback_query(F.data == "random_value")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer(str(randint(1, 10)))
+
 
 # Хэндлер на команду /test1
 @dp.message(Command("test1"))
